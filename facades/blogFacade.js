233f04af8ecf72450e1b836d45a626ba111d4210
Gonron <lundsgaard1997@hotmail.dk>
2019-03-15 18:57:37 +0100

    fixes

--- facades/blogFacade.js
+++ facades/blogFacade.js
@@ -2,32 +2,32 @@
 var LocationBlog = require("../models/LocationBlog");
 
 function getAllBlogs() {
-    return LocationBlog.find({}).exec()
+  return LocationBlog.find({}).exec()
 }
 
 function addLocationBlog(info, img, longitude, latitude, author, created) {
-    return newBLog = LocationBlog({ info, img, pos: { longitude, latitude }, author, created }).save()
+  return newBLog = LocationBlog({ info, img, pos: { longitude, latitude }, author, created }).save()
 }
 
 function likeLocationBlog(id) {
-    return null
-    // Start by finding the desired blog
-    // Update it so the likedBy array has the users ID in it.
+  return null
+  // Start by finding the desired blog
+  // Update it so the likedBy array has the users ID in it.
 }
 
 function findByInfo(info) {
-    return LocationBlog.findOne({ info }).exec()
+  return LocationBlog.findOne({ info }).exec()
 }
 
 function findById(id) {
-    return LocationBlog.findById({ _id: id }).exec()
+  return LocationBlog.findById({ _id: id }).exec()
 }
 
 
 module.exports = {
-    findByInfo,
-    findById,
-    getAllBlogs,
-    addLocationBlog,
-    likeLocationBlog
+  findByInfo,
+  findById,
+  getAllBlogs,
+  addLocationBlog,
+  likeLocationBlog
 }
